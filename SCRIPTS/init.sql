4c202cddc3edb629fa368aa709872f4d2535339c
Tokelo Komane <komanetokelo@gmail.com>
2026-08-21 02:18:27 +0200

    Create init.sql
    -script run to create database and schemas that will hold the data 
    - if the database found and  it will kick the use back to SINGLE_USER mode..

---
+++ SCRIPTS/init.sql
@@ -0,0 +1,39 @@
+USE master; -- Opening the master for the database 
+-- CREATING DATABASE TO LOAD DATA
+GO
+
+-- Check if the database exists first
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'bicycledata ')
+BEGIN
+    -- This block only runs if the database is found
+    PRINT 'Database found. Kicking out users and switching to SINGLE_USER mode...';
+    
+    ALTER DATABASE bicycledata 
+    SET SINGLE_USER 
+    WITH ROLLBACK IMMEDIATE;
+END
+ELSE
+BEGIN
+    -- This block runs if the database does not exist
+    PRINT 'CREATING bicycledata database for this operation';
+    CREATE DATABASE bicycledata;
+END;
+-- CREATING SCHEMAS TO LOAD DATA AND CLEAN 
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'bronze')
+BEGIN
+    EXEC('CREATE SCHEMA bronze');
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'silver')
+BEGIN
+    EXEC('CREATE SCHEMA silver');
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'gold')
+BEGIN
+    EXEC('CREATE SCHEMA gold');
+END
+GO
+
