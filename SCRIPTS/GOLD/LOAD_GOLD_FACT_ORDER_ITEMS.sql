a95118ae39eee1e6cdb058bf20b0843a2e218779
Tokelo Komane <komanetokelo@gmail.com>
2026-08-24 17:06:29 +0200

    Update LOAD_GOLD_FACT_ORDER_ITEMS.sql

--- SCRIPTS/GOLD/LOAD_GOLD_FACT_ORDER_ITEMS.sql
+++ SCRIPTS/GOLD/LOAD_GOLD_FACT_ORDER_ITEMS.sql
@@ -1,1 +1,12 @@
-SELECT * INTO gold.fact_order_items FROM silver.order_items
+SELECT * INTO gold.fact_order_items FROM
+(SELECT
+	i.order_id,
+	i.item_id,
+	i.product_id,
+	i.quantity,
+	i.list_price,
+	i.discount,
+	i.quantity * i.list_price AS sales_amount,
+	ROUND(i.quantity * i.list_price*(1-i.discount),2) as  discount_amount
+
+FROM silver.order_items i) i
