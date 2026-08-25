4c6dedd62543ed5a0c943bebc5aeca21ad966c5a
Tokelo Komane <komanetokelo@gmail.com>
2026-08-26 00:14:54 +0200

    Update full_table_report.sql

--- SQL_EDA_BUSINESS/full_table_report.sql
+++ SQL_EDA_BUSINESS/full_table_report.sql
@@ -1,4 +1,4 @@
-CREATE VIEW eda_full_report AS 
+SELECT * INTO gold.business_report FROM (
 SELECT DISTINCT
 	i.order_id,
 	i.item_id,
@@ -20,8 +20,38 @@ SELECT DISTINCT
 	p.model_year,
 	p.list_price,	
 	o.order_date,
+	DATEPART(DAY,o.order_date) AS order_day,
+	DATENAME(weekday, o.order_date) AS order_dayname,
+	CONCAT('Q',DATEPART(quarter, o.order_date)) AS order_quarter,
+	DATENAME(MONTH,o.order_date) AS order_month,
+	DATEPART(YEAR,o.order_date) AS  order_year,
 	o.require_date,
-	o.shipped_date,
+	DATEPART(DAY,o.require_date) AS required_day,
+	DATENAME(weekday, o.require_date) AS requires_dayname,
+	CONCAT('Q',DATEPART(quarter, o.require_date)) AS required_quarter,
+	DATENAME(MONTH,o.require_date) AS required_month, 
+	DATEPART(YEAR,o.require_date) AS required_year,
+	CASE WHEN o.shipped_date IS NULL THEN o.require_date
+	ELSE o.shipped_date
+	END shipped_date,
+	CASE WHEN o.shipped_date IS NULL THEN DATEPART(DAY,o.require_date)
+	ELSE DATEPART(DAY,o.shipped_date)
+	END shipped_day,
+	CASE WHEN o.shipped_date IS NULL THEN DATENAME(weekday,o.require_date)
+	ELSE DATENAME(weekday,o.shipped_date)
+	END shipped_dayname,
+	CASE WHEN o.shipped_date IS NULL THEN CONCAT('Q',DATEPART(QUARTER,o.require_date))
+	ELSE CONCAT('Q',DATEPART(QUARTER,o.shipped_date))
+	END shipped_quarter,
+	CASE WHEN o.shipped_date IS NULL THEN DATENAME(MONTH,o.require_date)
+	ELSE DATENAME(MONTH,o.shipped_date)
+	END shipped_month,
+	CASE WHEN o.shipped_date IS NULL THEN DATEPART(YEAR,o.require_date)
+	ELSE DATEPART(YEAR,o.shipped_date)
+	END shipped_year,
+	CASE WHEN DATEDIFF(DAY,o.shipped_date,o.require_date) < 0 THEN 'Late'
+	ELSE 'On_time'
+	END delivery_time,
 	SUM(p.quantity)  OVER( PARTITION BY i.order_id,i.item_id, o.customer_fullname, o.staff_fullname, o.store_name, p.product_name, 
 		p.category_name, p.gender_based, p.model_year, p.list_price, o.order_date, 
 		o.require_date, o.shipped_date ) AS stock_qty,
@@ -45,3 +75,4 @@ LEFT JOIN gold.dim_products p
 ON i.product_id = p.product_id
 LEFT JOIN gold.dim_orders o
 ON o.order_id = i.order_id
+) h;
